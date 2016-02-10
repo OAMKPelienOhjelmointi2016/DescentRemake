8c483a4f9765615e60dbbc2b3fe77cf249cd7e7c
Kasperi Klaavo <kasperi.klaavo@outlook.com>
2016-02-10 17:53:34 +0200

    Added missile explosion soundeffect

--- DescentRemake/Assets/Scripts/MissileExplosionFunc.cs
+++ DescentRemake/Assets/Scripts/MissileExplosionFunc.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class MissileExplosionFunc : MonoBehaviour {
 
+    public float volume;
+    public AudioClip explosionSoundeffect;
+
 	void Start () {
+        AudioSource.PlayClipAtPoint(explosionSoundeffect, transform.position, volume);
         GameObject.Destroy(this.gameObject, .18f);
 	}
 	
